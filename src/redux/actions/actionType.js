6baca820aae499847f601a1e1b6901e9f9870b1d
Pedro Luiz <pedroluizlsg@gmail.com>
2022-09-08 17:30:46 -0300

    Requisito 2 finalizado

---
+++ src/redux/actions/actionType.js
@@ -0,0 +1,2 @@
+export const LOG_REQUEST = 'LOG_REQUEST';
+export const LOG_RESPONSE = 'LOG_RESPONSE';
--- src/redux/reducers/token.js
+++ src/redux/reducers/token.js
@@ -1,3 +1,5 @@
+import { LOG_REQUEST, LOG_RESPONSE } from '../actions/actionType';
+
 const INITIAL_STATE = {
   response_code: 0,
   response_message: '',
@@ -5,7 +7,16 @@ const INITIAL_STATE = {
 };
 
 function token(state = INITIAL_STATE, action) {
-  switch (action.payload) {
+  switch (action.type) {
+  case LOG_REQUEST:
+    return { ...state };
+  case LOG_RESPONSE:
+    return {
+      ...state,
+      response_code: action.payload.response_code,
+      response_message: action.payload.response_message,
+      token: action.payload.token,
+    };
   default:
     return state;
   }
