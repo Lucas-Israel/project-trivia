b5b28f2e11849d42075f7b2cf7bf7a5cc0813036
Lucas Israel <lisraelalves92@gmail.com>
2022-09-10 10:55:22 -0300

    req7 feito

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -3,28 +3,53 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class Questions extends Component {
+  state = {
+    showAnswers: false,
+    answer: [],
+    resultIndex: 0,
+  };
+
+  componentDidMount() {
+    this.sortingAnswers();
+  }
+
+  sortingAnswers = () => {
+    const { results } = this.props;
+    const { resultIndex } = this.state;
+    const inputForRandom = 0.5;
+    const answer = results[resultIndex].incorrect_answers
+      .concat(results[resultIndex].correct_answer);
+    answer.sort(() => Math.random() - inputForRandom);
+    this.setState({ answer });
+  };
+
+  showAnswersHandler = () => {
+    this.setState({ showAnswers: true });
+  };
+
   render() {
     const { results } = this.props;
-    const indexDaPergunta = 0;
-    const inputForRandom = 0.5;
-    const answer = results[0].incorrect_answers
-      .concat(results[indexDaPergunta].correct_answer);
+    const { showAnswers, answer, resultIndex } = this.state;
     return (
-      <div>
-        <div data-testid="question-category">{results[indexDaPergunta].category}</div>
-        <div data-testid="question-text">
-          {results[indexDaPergunta].question
-            .replaceAll(/&#039;/g, '\'').replaceAll(/&\w*.;/g, '"')}
+      <div className="game">
+        <div className="game-question">
+          <div data-testid="question-category">{results[resultIndex].category}</div>
+          <div data-testid="question-text">
+            {results[resultIndex].question
+              .replaceAll(/&#039;/g, '\'').replaceAll(/&\w*.;/g, '"')}
 
+          </div>
         </div>
-        <div data-testid="answer-options">
-          {answer.sort(() => Math.random() - inputForRandom)
-            .map((question, i) => (question === results[indexDaPergunta].correct_answer
+        <div className="game-answers">
+          <div data-testid="answer-options">
+            {answer.map((question, i) => (question === results[resultIndex].correct_answer
               ? (
                 <button
                   type="button"
                   data-testid="correct-answer"
                   key={ i }
+                  style={ { border: showAnswers && '3px solid rgb(6, 240, 15)' } }
+                  onClick={ this.showAnswersHandler }
                 >
                   {question.replaceAll(/&#039;/g, '\'').replaceAll(/&eacute;/g, 'é').replaceAll(/&\w*.;/g, '"')}
                 </button>
@@ -32,13 +57,16 @@ class Questions extends Component {
               : (
                 <button
                   type="button"
-                  data-testid={ `wrong-answer-${results[indexDaPergunta].incorrect_answers
+                  data-testid={ `wrong-answer-${results[resultIndex].incorrect_answers
                     .indexOf(question)}` }
                   key={ i }
+                  style={ { border: showAnswers && '3px solid red' } }
+                  onClick={ this.showAnswersHandler }
                 >
                   {question.replaceAll(/&#039;/g, '\'').replaceAll(/&eacute;/g, 'é').replaceAll(/&\w*.;/g, '"')}
                 </button>
               )))}
+          </div>
         </div>
       </div>
     );
