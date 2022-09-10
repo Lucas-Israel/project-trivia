bca37526aca6159979499a42ea937c5b83cdeac6
Lucas Israel <lisraelalves92@gmail.com>
2022-09-10 10:59:17 -0300

    arrumado erro

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
-export class Feedback extends Component {
+class Feedback extends Component {
   render() {
     const { score, assertions } = this.props;
     const correctAnswer = 3;
